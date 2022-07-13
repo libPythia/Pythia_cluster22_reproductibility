cc8b30a5931b4415226cde28772436adb82c5166
François Trahay <francois.trahay@telecom-sudparis.eu>
2022-07-13 15:28:43 +0200

    installation scripts for pythia

--- pythia/install_pythia.sh
+++ pythia/install_pythia.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-# get Pythia
-COMMIT_HASH=
-git clone TODO pythia
-cd pythia
-git reset --hard $COMMIT_HASH
+PREFIX=$PWD
+BUILDDIR=$PWD
+PYTHIA_URL="https://github.com/libPythia/pythia.git"
+COMMIT_HASH=f353a754bcc1fe6c525a14f81ae3c4faf9967856
 
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+OPTIONS:
+   -h                               Show this message
+   -p <director>		    Path to the installation directory
+   -b <directory>	       	    Path to the build directory
+EOF
+}
+while getopts 'p:b:h' OPTION; do
+  case $OPTION in
+  p)
+	PREFIX=$OPTARG
+	;;
+  b)
+	BUILDDIR=$OPTARG
+	;;
+  h)	usage
+	exit 2
+	;;
+  esac
+done
+
+# remove the options from the command line
+shift $(($OPTIND - 1))
+
+# get Pythia
+cd "$BUILDDIR"
+git clone "$PYTHIA_URL" pythia || exit 1
+cd pythia || exit 1
+git reset --hard $COMMIT_HASH || exit 1
 
 # install pythia
-# TODO
+meson build -Dprefix="$PREFIX" || exit 1
+cd build || exit 1
+ninja install || exit 1
 
